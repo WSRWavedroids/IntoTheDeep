3c3e4a2c6ead79f1d0cd2e81fd77a4073777949b
WSRWaveDroids <wavedroid13206@gmail.com>
2024-09-10 20:55:19 -0500

    Added a basic strafer script for the Rock em sock ems

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/Basic_Strafer_Bot.java
@@ -0,0 +1,185 @@
+package org.firstinspires.ftc.teamcode.Outreach;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import java.util.Objects;
+
+public class Basic_Strafer_Bot {
+
+    public DcMotor frontLeftDrive;
+    public DcMotor frontRightDrive;
+    public DcMotor backLeftDrive;
+    public DcMotor backRightDrive;
+
+
+    public Telemetry telemetry;
+    //public BNO055IMU imu;
+
+    //init and declare war
+    public OpMode opmode;
+    public HardwareMap hardwareMap;
+    public static double parkingZone;
+    public String startingPosition;
+    public String controlMode = "Robot Centric";
+
+    public Basic_Strafer_Bot()
+    {
+
+    }
+    //Initialize motors and servos
+    public Basic_Strafer_Bot(HardwareMap hardwareMap, Telemetry telemetry, OpMode opmode){
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+        this.opmode = opmode;
+
+        // This section turns the names of the pieces of hardware into variables that we can program with.
+        // Make sure that the device name is the exact same thing you typed in on the configuration on the driver hub.
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+
+        //add arms to map
+        /*
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+        */
+        // This section sets the direction of all of the motors. Depending on the motor, this may change later in the program.
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD); //Was inverted as forward
+        // This tells the motors to chill when we're not powering them.
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        //This is new..
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+
+    public boolean isWheelsBusy(){
+        return backLeftDrive.isBusy() || frontLeftDrive.isBusy() || frontRightDrive.isBusy() || backRightDrive.isBusy();
+    }
+
+    public void stopAllMotors() {
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+    }
+
+    public void setTargets(String direction, int ticks) {
+
+        if (Objects.equals(direction, "Right")){
+            frontLeftDrive.setTargetPosition(-ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks + backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks + backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Left"){
+            frontLeftDrive.setTargetPosition(ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks + backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks + backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Forward"){
+            frontLeftDrive.setTargetPosition(-ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks - backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Backward") {
+            frontLeftDrive.setTargetPosition(ticks - frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(ticks - frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks - backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Turn Right") {
+            frontLeftDrive.setTargetPosition(ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks - frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks - backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Turn Left") {
+            frontLeftDrive.setTargetPosition(-ticks - frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks - backRightDrive.getCurrentPosition());
+
+        }
+
+
+
+
+        /*else if (direction == "Turntable"){
+            robot.open
+            armR.setTargetPosition(-ticks + armR.getCurrentPosition());
+        }*/
+
+    }
+
+    public void positionRunningMode(){
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void powerSet(double speed) {
+        frontLeftDrive.setPower(speed);
+        frontRightDrive.setPower(speed);
+        backLeftDrive.setPower(speed);
+        backRightDrive.setPower(speed);
+
+    }
+
+    public void encoderRunningMode(){
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void encoderReset(){
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    @SuppressLint("DefaultLocale")
+    public void tellMotorOutput(){
+        telemetry.addData("Control Mode", controlMode);
+        telemetry.addData("Motors", String.format("FL Power(%.2f) FL Location (%d) FL Target (%d)", frontLeftDrive.getPower(), frontLeftDrive.getCurrentPosition(), frontLeftDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("FR Power(%.2f) FR Location (%d) FR Target (%d)", frontRightDrive.getPower(), frontRightDrive.getCurrentPosition(), frontRightDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("BL Power(%.2f) BL Location (%d) BL Target (%d)", backLeftDrive.getPower(), backLeftDrive.getCurrentPosition(), backLeftDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("BR Power(%.2f) BR Location (%d) BR Target (%d)", backRightDrive.getPower(), backRightDrive.getCurrentPosition(), backRightDrive.getTargetPosition()));
+        telemetry.update();
+    }
+
+    public double inchesToTicks(double inches){
+        // returns the inches * ticks per rotation / wheel circ
+        return ((inches/12.25) * 537.6 / .5);
+        //todo Reference that 1 inch ~= 50 ticks
+    }
+    // one side may be backwards due to the direction that the motor was faced
+
+    ElapsedTime timer = new ElapsedTime();
+
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/Basic_Strafer_Bot_Tele_Op.java
@@ -0,0 +1,253 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode.Outreach;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.Robot;
+
+
+/**
+ * This file is our iterative (Non-Linear) "OpMode" for TeleOp.
+ * An OpMode is a 'program' that runs in either the autonomous or the teleop period of an FTC match.
+ * The names of OpModes appear on the menu of the FTC Driver Station.
+ * When an selection is made from the menu, the corresponding OpMode
+ * class is selected on the Robot Controller and executed.
+ * This particular one is called "Lean Mean TeleOp Machine". I had a little too much fun with naming this.
+ *
+ * This OpMode controls the functions of the robot during the driver-controlled period.
+ *
+ * If the "@Disabled" line is not commented out, the program will not show up on the driver hub.
+ * If you ever have problems with the program not showing up on the driver hub, it's probably because of that.
+ *
+ * Throughout this program, there are comments explaining what everything does because previous programmers
+ * did a horrible job of doing that.
+ */
+
+@TeleOp(name="Basic Strafer", group="CompBot")
+public class Basic_Strafer_Bot_Tele_Op extends OpMode {
+
+    // This section tells the program all of the different pieces of hardware that are on our robot that we will use in the program.
+    private ElapsedTime runtime = new ElapsedTime();
+    private double speed = 0.75;
+    //private double storedSpeed;
+    public org.firstinspires.ftc.teamcode.Outreach.Basic_Strafer_Bot Bot = new Basic_Strafer_Bot();
+
+    /*
+     * Code to run ONCE when the driver hits INIT
+     */
+    public void init() {
+
+        // Call the initialization protocol from the Robot class.
+        Bot = new Basic_Strafer_Bot(hardwareMap, telemetry, this);
+
+        // Tell the driver that initialization is complete.
+        telemetry.addData("Status", "Initialized");
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits INIT, but before they hit PLAY
+     */
+    public void init_loop() {telemetry.addData("HYPE", "ARE! YOU! READY?!?!?!?!");}
+
+    /*
+     * Code to run ONCE when the driver hits PLAY
+     */
+    public void start() {
+        runtime.reset();
+        telemetry.addData("HYPE", "Let's do this!!!");
+        gamepad1.setLedColor(0, 0, 255, 100000000);
+        gamepad2.setLedColor(0, 0, 255, 100000000);
+    }
+
+    /*
+     * Code to run REPEATEDLY after the driver hits PLAY but before they hit STOP
+     */
+    public void loop() {
+
+        singleJoystickDrive();
+        // This little section updates the driver hub on the runtime and the motor powers.
+        // It's mostly used for troubleshooting.
+        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        Bot.tellMotorOutput();
+
+        float armStickY = this.gamepad2.left_stick_y;
+        float turntableStickX = this.gamepad2.right_stick_x;
+
+        // This section checks what buttons on the Dpad are being pressed and changes the speed accordingly.
+        //So Begins the input chain. At least try a bit to organise by driver
+
+        //Driver 1
+        if (gamepad1.back) {
+            if (Bot.controlMode == "Robot Centric"){
+                Bot.controlMode = "Field Centric";
+                telemetry.addData("Control Mode", "Field Centric Controls");
+            } else if (Bot.controlMode == "Field Centric"){
+                Bot.controlMode = "Robot Centric";
+                telemetry.addData("Control Mode", "Robot Centric Controls");
+            }
+        }
+
+        if (gamepad1.dpad_up) {
+            speed = 1;
+        } else if (gamepad1.dpad_down) {
+            speed = 0.25;
+        } else if (gamepad1.dpad_left) {
+            speed = 0.5;
+        } else if (gamepad1.dpad_right) {
+            speed = 0.75;
+        }
+
+        if (speed == 1) {
+            telemetry.addData("Speed", "Fast Boi");
+        } else if (speed == 0.5) {
+            telemetry.addData("Speed", "Slow Boi");
+        } else if (speed == 0.25) {
+            telemetry.addData("Speed", "Super Slow Boi");
+        } else if (speed == 0.75) {
+            telemetry.addData("Speed", "Normal Boi");
+        }
+
+
+
+
+        //Beginning of fast turn
+
+        if(gamepad1.right_trigger >= 0.5)
+        {
+            //storedSpeed = speed;
+            speed = 1;
+            //Do something
+            //speed = storedSpeed;
+
+        }
+        else if (gamepad1.left_trigger >0.5)
+        {
+            //storedSpeed = speed;
+            speed = 0.50;
+            //Do something
+            //speed = storedSpeed;
+        }
+        //
+
+    }
+
+    /*
+     * Code to run ONCE after the driver hits STOP
+     */
+    public void stop () { telemetry.addData("Status", "Robot Stopped"); }
+
+
+    /*
+     * The holding cell for all of the random functions we call above.
+     */
+
+    public void setIndividualPowers ( float[] motorPowers){
+        // This function creates an array so that the function below works.
+        // Don't mess with this function unless you know what you're doing.
+
+        if (motorPowers.length != 4) {
+            return;
+        }
+        Bot.frontLeftDrive.setPower(-motorPowers[0]);
+        Bot.frontRightDrive.setPower(-motorPowers[1]);
+        Bot.backLeftDrive.setPower(-motorPowers[2]);
+        Bot.backRightDrive.setPower(-motorPowers[3]);
+    }
+
+    private void singleJoystickDrive () {
+        // We don't really know how this function works, but it makes the wheels drive, so we don't question it.
+        // Don't mess with this function unless you REALLY know what you're doing.
+
+        float rightX = -this.gamepad1.right_stick_x;
+        float leftY = this.gamepad1.left_stick_y;
+        float leftX = -this.gamepad1.left_stick_x;
+
+        double leftStickAngle = Math.atan2(leftY, leftX);
+        double leftStickMagnitude = Math.sqrt(leftX * 2.0 + leftY * 2.0);
+        //double robotAngle = robot.imu.getAngularOrientation(AxesReference.EXTRINSIC, AxesOrder.XYZ, AngleUnit.DEGREES).firstAngle;
+
+        if (leftStickMagnitude > 1){
+            leftStickMagnitude = 1;
+        }
+
+        float[] motorPowers = new float[4];
+
+        if (Bot.controlMode == "Robot Centric") {
+
+            motorPowers[0] = (leftY + leftX + rightX);
+            motorPowers[1] = (leftY - leftX - rightX);
+            motorPowers[2] = (leftY - leftX + rightX);
+            motorPowers[3] = (leftY + leftX - rightX);
+
+        } else if (Bot.controlMode == "Field Centric") {
+            /*
+            motorPowers[0] = (float) (Math.sin(leftStickAngle + 45 - robotAngle) * leftStickMagnitude + rightX);
+            motorPowers[1] = (float) (Math.sin(leftStickAngle - 45 - robotAngle) * leftStickMagnitude + rightX);
+            motorPowers[2] = (float) (Math.sin(leftStickAngle - 45 - robotAngle) * leftStickMagnitude + rightX);
+            motorPowers[3] = (float) (Math.sin(leftStickAngle + 45 - robotAngle) * leftStickMagnitude + rightX);
+            */
+        }
+
+        float max = getLargestAbsVal(motorPowers);
+        if (max < 1) {
+            max = 1;
+        }
+
+        for (int i = 0; i < motorPowers.length; i++) {
+            motorPowers[i] *= (speed / max);
+
+            float abs = Math.abs(motorPowers[i]);
+            if (abs < 0.05) {
+                motorPowers[i] = 0.0f;
+            }
+            if (abs > 1.0) {
+                motorPowers[i] /= abs;
+            }
+        }
+
+        setIndividualPowers(motorPowers);
+
+    }
+
+    private float getLargestAbsVal ( float[] values){
+        // This function does some math!
+        float max = 0;
+        for (float val : values) {
+            if (Math.abs(val) > max) {
+                max = Math.abs(val);
+            }
+        }
+        return max;
+    }
+
+}
