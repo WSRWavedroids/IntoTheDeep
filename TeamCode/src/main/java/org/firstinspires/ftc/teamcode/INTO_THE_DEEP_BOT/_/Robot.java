98d1ec51e3cfaa5642acd17a079d21595418dc58
ClaireToTheC0da <turtletastic998@gmail.com>
2024-12-03 14:12:56 -0600

    Added IMU Parameters in

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/Robot.java
@@ -0,0 +1,364 @@
+package org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._;
+
+import android.annotation.SuppressLint;
+
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.hardware.ImuOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.ftccommon.internal.manualcontrol.parameters.ImuParameters;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
+import java.lang.reflect.Array;
+import java.util.Objects;
+
+public class Robot {
+
+    public DcMotor frontLeftDrive;
+    public DcMotor frontRightDrive;
+    public DcMotor backLeftDrive;
+    public DcMotor backRightDrive;
+    public DcMotor lifty;
+    public DcMotor waterslide;
+
+
+
+    public CRServo leftIntake;
+    public CRServo rightIntake;
+
+    public Servo intakeFlipper;
+
+    public Servo flippyOutakeServo;
+
+    public Servo grabbyOutakeServo;
+
+
+
+    //public DistanceSensor distanceSensor;
+
+
+    //public WebcamName CamCam;
+
+    public Telemetry telemetry;
+    //public BNO055IMU imu;
+
+    //init and declare war
+    public OpMode opmode;
+    public HardwareMap hardwareMap;
+    public static double parkingZone;
+    public String startingPosition;
+    public String controlMode = "Robot Centric";// Robot Centric
+    public String intakeFlipperPos ="UP";
+    public IMU.Parameters imuParameters;
+
+    //Initialize motors and servos
+    public Robot(HardwareMap hardwareMap, Telemetry telemetry, OpMode opmode){
+        this.hardwareMap = hardwareMap;
+        this.telemetry = telemetry;
+        this.opmode = opmode;
+
+        // This section turns the names of the pieces of hardware into variables that we can program with.
+        // Make sure that the device name is the exact same thing you typed in on the configuration on the driver hub.
+        frontRightDrive = hardwareMap.get(DcMotor.class, "frontRightDrive");
+        frontLeftDrive = hardwareMap.get(DcMotor.class, "frontLeftDrive");
+        backLeftDrive = hardwareMap.get(DcMotor.class, "backLeftDrive");
+        backRightDrive = hardwareMap.get(DcMotor.class, "backRightDrive");
+        lifty = hardwareMap.get(DcMotor.class, "lifty");
+        waterslide = hardwareMap.get(DcMotor.class, "waterslide");
+        leftIntake = hardwareMap.get(CRServo.class, "leftIntake");
+        rightIntake = hardwareMap.get(CRServo.class, "rightIntake");
+        intakeFlipper = hardwareMap.get(Servo.class, "flipperServo");
+        flippyOutakeServo = hardwareMap.get(Servo.class, "flippyOutakeServo");
+        grabbyOutakeServo = hardwareMap.get(Servo.class, "grabbyOutakeServo");
+
+        imuParameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(
+                        RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                        RevHubOrientationOnRobot.UsbFacingDirection.RIGHT
+                )
+        );
+
+        // This section sets the direction of all of the motors. Depending on the motor, this may change later in the program.
+        frontLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontRightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backLeftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backRightDrive.setDirection(DcMotor.Direction.FORWARD); //Was inverted as forward
+        lifty.setDirection(DcMotor.Direction.REVERSE);//inverted
+        waterslide.setDirection(DcMotor.Direction.FORWARD);
+
+        // This tells the motors to chill when we're not powering them.
+        frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        backRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        waterslide.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //This is new..
+        telemetry.addData("Status", "Initialized");
+
+    }
+
+
+    public boolean isWheelsBusy(){
+        return backLeftDrive.isBusy() || frontLeftDrive.isBusy() || frontRightDrive.isBusy() || backRightDrive.isBusy();
+    }
+
+    public void stopAllMotors() {
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        backLeftDrive.setPower(0);
+        backRightDrive.setPower(0);
+    }
+
+    public void setTargets(String direction, int ticks) {
+
+        if (Objects.equals(direction, "Right")){
+            frontLeftDrive.setTargetPosition(-ticks + frontLeftDrive.getCurrentPosition());
+            frontLeftDrive.getTargetPosition();
+            frontRightDrive.setTargetPosition(ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks + backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks + backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Left"){
+            frontLeftDrive.setTargetPosition(ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks + backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks + backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Forward"){
+            frontLeftDrive.setTargetPosition(-ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks - backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Backward") {
+            frontLeftDrive.setTargetPosition(ticks - frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(ticks - frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks - backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Turn Right") {
+            frontLeftDrive.setTargetPosition(ticks + frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(-ticks - frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(-ticks - backRightDrive.getCurrentPosition());
+
+        } else if (direction == "Turn Left") {
+            frontLeftDrive.setTargetPosition(-ticks - frontLeftDrive.getCurrentPosition());
+            frontRightDrive.setTargetPosition(ticks + frontRightDrive.getCurrentPosition());
+            backLeftDrive.setTargetPosition(-ticks - backLeftDrive.getCurrentPosition());
+            backRightDrive.setTargetPosition(ticks - backRightDrive.getCurrentPosition());
+
+        }
+
+
+
+
+    }
+
+    public void positionRunningMode(){
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        lifty.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        waterslide.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void powerSet(double speed) {
+        frontLeftDrive.setPower(speed);
+        frontRightDrive.setPower(speed);
+        backLeftDrive.setPower(speed);
+        backRightDrive.setPower(speed);
+
+    }
+
+    public void intake_spin (double direction){
+        //servos spin in thingy
+        if(direction > 0)
+        {
+            leftIntake.setDirection(CRServo.Direction.FORWARD);
+            rightIntake.setDirection(CRServo.Direction.REVERSE);//direction is forward... set speed to that val
+            leftIntake.setPower(Math.abs(direction));
+            rightIntake.setPower(Math.abs(direction));
+        }
+        else
+        {
+            leftIntake.setDirection(CRServo.Direction.REVERSE);
+            rightIntake.setDirection(CRServo.Direction.FORWARD);//direction is forward... set speed to that val
+            leftIntake.setPower(Math.abs(direction));
+            rightIntake.setPower(Math.abs(direction));
+        }
+
+    }
+    public boolean canWiggle = true;
+    public void intakePosition (String intakeFlipperPos)
+    {
+        if(intakeFlipperPos == "UP")
+        {
+            intakeFlipper.setPosition(1);//guess position
+            canWiggle = true;
+        }
+        else if(intakeFlipperPos == "DOWN")
+        {
+            intakeFlipper.setPosition(0);
+            canWiggle = true;
+        }
+
+    }
+
+    public void outakeclawOpenClose(String state)
+    {
+        if(state == "OPEN")
+        {
+           grabbyOutakeServo.setPosition(.83);
+        }
+        else if (state == "CLOSED")
+        {
+            grabbyOutakeServo.setPosition(.95);
+        }
+    }
+
+    public void tempOutakePos(String pos)
+    {
+        if (pos == "DOWN")
+        {
+            flippyOutakeServo.setPosition(.5);
+        }
+        if (pos == "UP")
+        {
+            flippyOutakeServo.setPosition(.8);
+        }
+    }
+
+    public void encoderRunningMode(){
+        frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+    }
+
+    public void encoderReset(){
+        frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    @SuppressLint("DefaultLocale")
+    public void tellMotorOutput(){
+        telemetry.addData("Control Mode", controlMode);
+        telemetry.addData("Motors", String.format("FL Power(%.2f) FL Location (%d) FL Target (%d)", frontLeftDrive.getPower(), frontLeftDrive.getCurrentPosition(), frontLeftDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("FR Power(%.2f) FR Location (%d) FR Target (%d)", frontRightDrive.getPower(), frontRightDrive.getCurrentPosition(), frontRightDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("BL Power(%.2f) BL Location (%d) BL Target (%d)", backLeftDrive.getPower(), backLeftDrive.getCurrentPosition(), backLeftDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("BR Power(%.2f) BR Location (%d) BR Target (%d)", backRightDrive.getPower(), backRightDrive.getCurrentPosition(), backRightDrive.getTargetPosition()));
+        telemetry.addData("Motors", String.format("Lifty Power (%.2f) Lifty Location (%d) Lifty Target (%d)", lifty.getPower(), lifty.getCurrentPosition(), lifty.getTargetPosition()));
+        telemetry.addData("Motors", String.format("WaterSlide Motor Power (%.2f) WaterSlide Location (%d) WaterSlide Target (%d)", waterslide.getPower(), waterslide.getCurrentPosition(), waterslide.getTargetPosition()));
+        telemetry.addData("Flipper", intakeFlipper.getPosition());
+        telemetry.update();
+    }
+
+    public double inchesToTicks(double inches){
+        // returns the inches * ticks per rotation / wheel circ
+        return ((inches/12.25) * 537.6 / .5);
+        //todo Reference that 1 inch ~= 50 ticks
+    }
+    // one side may be backwards due to the direction that the motor was faced
+    public void moveArm(String direction){
+        if (direction == "Up"){
+            lifty.setPower(1);
+            lifty.setDirection(DcMotor.Direction.FORWARD);//inverted
+        } else if (direction == "Down"){
+            lifty.setPower(0.25);
+            lifty.setDirection(DcMotor.Direction.REVERSE);//Inverted
+        }
+    }
+
+    ElapsedTime timer = new ElapsedTime();
+
+    public void holdArm(){
+        lifty.setDirection(DcMotor.Direction.FORWARD);//
+        lifty.setPower(0.05);
+    }
+
+
+    public boolean primaryClawClosed = false;
+
+
+  /*  Some April Tag and tensorflow stuff
+
+    public void showersAndFlowers(){
+
+        AprilTagProcessor OSHAmobile;
+
+        OSHAmobile = new AprilTagProcessor.Builder()
+                .setTagLibrary(AprilTagGameDatabase.getCurrentGameTagLibrary())
+                .setDrawTagID(true)
+                .setDrawTagOutline(true)
+                .setDrawAxes(true)
+                .setDrawCubeProjection(true)
+                .build();
+    }
+
+    public void tensorFlowDetection(){
+
+        TfodProcessor safetyGlasses;
+
+        safetyGlasses = new TfodProcessor.Builder()
+                .setMaxNumRecognitions(10)
+                .setUseObjectTracker(true)
+                .setTrackerMaxOverlap((float) 0.2)
+                .setTrackerMinSize(16)
+                .build();
+    }
+
+    public void visionPortal(AprilTagProcessor aprilTagProcessor, TfodProcessor tfodProcessor){
+        VisionPortal Oracle;
+
+
+        myVisionPortal = new VisionPortal.Builder()
+                .setCamera(hardwareMap.get(WebcamName.class, "Cam Cam"))
+                .addProcessor(aprilTagProcessor)
+                .setCameraResolution(new Size(640, 480))
+                .setStreamFormat(VisionPortal.StreamFormat.YUY2)
+                .enableCameraMonitoring(true)
+                .setAutoStopLiveView(true)
+                .build();
+
+
+    }
+
+    public void retrieveAprilTags(AprilTagProcessor ATP){
+        List<AprilTagDetection> ATDS;         // list of all detections // current detection in for() loop
+        int SPOTnum;                           // ID code of current detection, in for() loop
+
+        // Get a list of AprilTag detections.
+        ATDS = ATP.getDetections();
+
+        // Cycle through through the list and process each AprilTag.
+        for (AprilTagDetection SPOT : ATDS) {
+
+            if (SPOT.metadata != null) {  // This check for non-null Metadata is not needed for reading only ID code.
+                SPOTnum = SPOT.id;
+
+                // Now take action based on this tag's ID code, or store info for later action.
+
+            }
+        }
+    }
+
+
+   */
+
+
+
+}
