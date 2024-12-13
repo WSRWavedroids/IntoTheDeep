a1df7e1bf195fb244214cea3f880266ca315a225
ClaireToTheC0da <turtletastic998@gmail.com>
2024-12-13 16:46:05 -0600

    Okay it's actually correctly inverted now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/Teleop/Basic_TeleOp_NewBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/Teleop/Basic_TeleOp_NewBot.java
@@ -283,9 +283,9 @@ public class Basic_TeleOp_NewBot extends OpMode {
     private void singleJoystickDrive () {
         // We don't really know how this function works, but it makes the wheels drive, so we don't question it.
         // Don't mess with this function unless you REALLY know what you're doing.
-        float leftY = this.gamepad1.left_stick_y;
+        float leftY = -this.gamepad1.left_stick_y;
         float rightX = this.gamepad1.right_stick_x;
-        float leftX = -this.gamepad1.left_stick_x;
+        float leftX = this.gamepad1.left_stick_x;
 
         double leftStickAngle = Math.atan2(leftY, leftX);
         double leftStickMagnitude = Math.sqrt(leftX * 2.0 + leftY * 2.0);
@@ -299,10 +299,10 @@ public class Basic_TeleOp_NewBot extends OpMode {
 
         if (robot.controlMode == "Robot Centric") {
 
-            motorPowers[0] = (leftY + leftX - rightX);//might need inverted back
-            motorPowers[1] = (leftY - leftX + rightX);
-            motorPowers[2] = (leftY - leftX - rightX);
-            motorPowers[3] = (leftY + leftX + rightX);
+            motorPowers[0] = (leftY + leftX + rightX);//might need inverted back
+            motorPowers[1] = (leftY - leftX - rightX);
+            motorPowers[2] = (leftY - leftX + rightX);
+            motorPowers[3] = (leftY + leftX - rightX);
 
         } else if (robot.controlMode == "Field Centric") {
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
