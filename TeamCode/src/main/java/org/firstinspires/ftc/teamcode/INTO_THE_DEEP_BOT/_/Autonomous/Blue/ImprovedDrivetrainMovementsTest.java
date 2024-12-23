eadc771f8533a51df54889e98071eae1b3e16bb9
WSRWaveDroids <wavedroid13206@gmail.com>
2024-12-23 15:17:09 -0600

    BIG AUTO PLUS UPDATE: Added: Time based drive systems in all directions, diagonal movement for both ticks and time, and the new dedicated function for the lift in auto. Also, I made at least 1 comment per function to explain what they do.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/Autonomous/Blue/ImprovedDrivetrainMovementsTest.java
@@ -0,0 +1,35 @@
+package org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.Autonomous.Blue;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+import org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.Autonomous.AutonomousPLUS;
+
+@Autonomous(group = "Basic", name = "Improved Movements?")
+public class ImprovedDrivetrainMovementsTest extends AutonomousPLUS {
+
+    public String currentPosition;
+    public String target;
+
+    public void runOpMode() {
+
+        super.runOpMode();
+
+        waitForStart();
+        robot.lifty.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.encoderReset();
+        robot.lifty.setPower(0);
+        robot.tempOutakePos("UP");
+        robot.intakePosition("UP");
+        robot.slidesIn();
+        speed = .65;
+        robot.outakeclawOpenClose("CLOSED");
+
+
+        timeTurnRight(2500, 2000);
+        timeTurnleft(2500, 2000);
+
+
+
+    }
+}
