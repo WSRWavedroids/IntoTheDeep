36d50f15209eb66359f4c27c91d459b4205da6fb
WSRWaveDroids <wavedroid13206@gmail.com>
2025-01-17 13:23:36 -0600

    We need to do other stuff now

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/Blue/BLUE_OBSZONE_HighChamber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/Blue/BLUE_OBSZONE_HighChamber.java
@@ -17,35 +17,36 @@ public class BLUE_OBSZONE_HighChamber extends AutonomousPLUS {
 
         waitForStart();
 
-        robot.liftyL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.liftyR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        robot.encoderReset();
-        robot.liftyL.setPower(0);
-        robot.liftyR.setPower(0);
-        robot.tempOutakePos("DOWN");
-        robot.intakePosition("UP");
-        robot.intakePosition("UP");
-        robot.slidesIn();
+        //First movements to line up under bar
+        prepareAuto();
         speed = .65;
-        robot.outakeclawOpenClose("CLOSED");
-        moveRobotForward(500, 2);
-        moveRobotLeft(350, 2);
-        moveArm(1500, 1, 2);
-        speed=.4;
-        moveRobotForward(400, 2);
+        moveDiagonalLeft(495, 0); //Trusting Pythagorus
+        moveRobotForward(150, 0);
 
-        moveArm(1850, 1, 2); //Places the starting specimen on the bar
+        //moveRobotForward(500, 0);
+        //moveRobotLeft(350, 0);
+        moveArm(1500, 1, 0);
+        moveRobotForward(400, 0);
+
+        moveArm(1850, 1, 0); //Places the starting specimen on the bar
         robot.outakeclawOpenClose("OPEN"); //Releases said specimen
 
-        speed = .6;
-        moveRobotBackward(500,500);
+
+        moveRobotBackward(500,500);//Back away
+
+        //go push the sample to the obszone
         speed = 1;
-        moveArm(0, 1, 2);
-        moveRobotRight(1575, 2);
-        speed = .5;
-        turnRobotRight(1530, 2);
-        speed = .3;
-        moveRobotForward(400, 2);
+        moveArm(143, 1, 2);
+        moveRobotRight(900, 2);
+        moveRobotForward(1000, 0);
+        moveRobotRight(450, 0);
+        moveRobotBackward(800, 0);
+        moveRobotForward(200, 0);
+
+        turnRobotRight(1530, 0);
+
+        moveRobotForward(400, 0);
+        robot.outakeclawOpenClose("CLOSE");
         /*robot.outakeclawOpenClose("CLOSED"); //This is the line that picks up the specimen from the field wall
         prepareNextAction(500);
         moveArm(230, 1, 2);
