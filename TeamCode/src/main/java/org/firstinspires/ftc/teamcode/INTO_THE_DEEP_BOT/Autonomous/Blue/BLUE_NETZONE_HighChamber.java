ff31dbc3d0c279248c3b19ef469ae3dad42b5071
WSRWaveDroids <wavedroid13206@gmail.com>
2025-01-17 11:20:02 -0600

    Pushing for reasons

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/Blue/BLUE_NETZONE_HighChamber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/Blue/BLUE_NETZONE_HighChamber.java
@@ -18,26 +18,27 @@ public class BLUE_NETZONE_HighChamber extends AutonomousPLUS {
 
         waitForStart();
         prepareAuto();
-        speed = .3;
+        speed = .6;
         sleep(200);
         moveRobotForward(500, 0);
-        speed=.6;
-        moveRobotRight(1100, 0);
+        moveRobotRight(950, 0);
 
         moveArm(1500, 1, 1000);
 
-        moveRobotForward(395, 0);
+        speed = .3;
+        moveRobotForward(365, 0);
+        speed = .6;
 
         moveArm(1800, 1, 0);
 
         robot.outakeclawOpenClose("OPEN");
         prepareNextAction(2);
-        moveRobotBackward(450,0);
+        moveRobotBackward(425,0);
         moveArm(0,1, 0);
-        moveRobotLeft(1550, 0);
+        moveRobotLeft(1385, 0);
         turnRobotRight(1500, 0);//
 
-        autoSlides(.70, 2000);
+        autoSlides(.6, 2000);
         robot.intake_spin(.5); //Pick up yellow sample
         robot.intakeFlipper.setPosition(.15);
         prepareNextAction(2000);
@@ -47,12 +48,23 @@ public class BLUE_NETZONE_HighChamber extends AutonomousPLUS {
 
         //robot.TransferSequence();
         turnRobotRight(450, 0);
-        moveRobotForward(250, 0);
+        moveRobotForward(300, 0);
         moveArm(1901, 1, 0);
         robot.tempOutakePos("UP");
         prepareNextAction(2000);
-        moveRobotBackward(300,0);
-        robot.TransferSequence(); // Collapses the robot so the robot doesn't fall when auto ends
+        moveRobotBackward(200,0);
+        robot.safeCollapse(); // Collapses the robot so the robot doesn't fall when auto ends
+
+        turnRobotLeft(460,0);
+        moveRobotRight(275, 0);
+        autoSlides(.65,2000);
+        robot.intake_spin(1); //Pick up other yellow sample
+        prepareNextAction(500);
+        robot.intakeFlipper.setPosition(.15);
         prepareNextAction(2000);
+        robot.intake_spin(0);
+        robot.TransferSequence();
+
+
     }
 }
