0ff16881f1c4c3d45297e2cc29c0848559f595b1
WSRWaveDroids <wavedroid13206@gmail.com>
2025-01-20 13:09:39 -0600

    Meet was... interesting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/Blue/BLUE_NETZONE_HighChamber.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/Blue/BLUE_NETZONE_HighChamber.java
@@ -18,39 +18,35 @@ public class BLUE_NETZONE_HighChamber extends AutonomousPLUS {
 
         waitForStart();
         prepareAuto();
+        setMotorTolerance(10);
 
         speed = .6;
         sleep(200);
 
         moveRobotForward(500, 0);
-        // This is the code to start on a different tile
-        //moveRobotRight(950, 0);
-
+        //moveRobotRight(950, 0); // This is the code to start on a different tile
         moveArm(1500, 1, 1000);
-
         speed = .3;
         moveRobotForward(350, 0);
-
         speed = .6;
         moveArm(1800, 1, 0);
-
         robot.outakeclawOpenClose("OPEN");
         prepareNextAction(2);
         moveRobotBackward(425,0);
         moveArm(0,1, 0);
-        moveRobotLeft(1385, 0);
-        turnRobotRight(1500, 0);//
 
-        autoSlides(.55, 2000);
+        // First sample
+        moveRobotLeft(1345, 0);
+        setMotorTolerance(8);
+        turnRobotRight(1530, 0);
+        setMotorTolerance(10);
+        autoSlides(.50, 2000);
         robot.intake_spin(.5); //Pick up yellow sample
-        prepareNextAction(500);
+        prepareNextAction(750);
         robot.intakeFlipper.setPosition(.15);
         prepareNextAction(2000);
         robot.intake_spin(0);
         robot.TransferSequence();
-        //robot.tempOutakePos("UP");
-
-        //robot.TransferSequence();
         turnRobotRight(450, 0);
         speed  = .3;
         moveArm(1901, 1, 0);
@@ -59,16 +55,21 @@ public class BLUE_NETZONE_HighChamber extends AutonomousPLUS {
         prepareNextAction(2000);
         moveRobotBackward(200,0);
         robot.safeCollapse(); // Collapses the robot so the robot doesn't fall when auto ends
+
+        /*// Second sample
         speed = .6;
+        setMotorTolerance(8);
         turnRobotLeft(460,0);
+        setMotorTolerance(10);
         moveRobotRight(275, 0);
         autoSlides(.65,2000);
         robot.intake_spin(1); //Pick up other yellow sample
-        prepareNextAction(500);
+        prepareNextAction(750);
         robot.intakeFlipper.setPosition(.15);
         prepareNextAction(2000);
         robot.intake_spin(0);
-        robot.TransferSequence();
+        robot.TransferSequence();*/
+        
         prepareNextAction(2000);
 
 
