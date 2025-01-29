525f9b2556ad74c4114428f7d8eb4416dcce89d2
WSRWaveDroids <wavedroid13206@gmail.com>
2025-01-29 15:01:27 -0600

    It works! Ready to merge into main.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/AutonomousPLUS.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Autonomous/AutonomousPLUS.java
@@ -368,14 +368,18 @@ public class AutonomousPLUS extends LinearOpMode {
     }
 
     public void pickupSample(int pickupTime, long pause) {
-        double distance = robot.leftSlide.getPosition() + .10;
+        // REQUIREMENTS TO USE FUNCTION (plz don't ignore):
+            // Needs a wait before the function if the most recent movement was the slides
+            // Function can't run until the servos get over their start-of-auto crisis (about 2.5 seconds). This is really obscure, but it might be a problem for somebody someday.
+        double distance = robot.rightSlide.getPosition() + .20;
         robot.intake_spin(.75);
         prepareNextAction(100);
         //robot.leftSlide.setPosition(1-(robot.leftSlide.getPosition()+.10));
         //robot.rightSlide.setPosition(0+(robot.leftSlide.getPosition()+.10));
         robot.intakeFlipper.setPosition(.15);
+        prepareNextAction(0);
         autoSlides(distance,0);
-        //prepareNextAction(pickupTime);
+        prepareNextAction(pickupTime);
         robot.intake_spin(0);
         prepareNextAction(pause);
     }
