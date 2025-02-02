17222d1248fa1191854e9d4c1e85c1bce4b095c9
WSRWaveDroids <wavedroid13206@gmail.com>
2025-02-02 11:14:09 -0600

    Stuff is very broken. I need to switch to main to update AutonomousPLUS.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/pedroPathing/Autonomous/stupid_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/pedroPathing/Autonomous/stupid_test.java
@@ -8,10 +8,15 @@ import com.pedropathing.pathgen.Path;
 import com.pedropathing.pathgen.PathChain;
 import com.pedropathing.pathgen.Point;
 import com.pedropathing.util.Timer;
+import com.pedropathing.util.Constants;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+
+import org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.pedroPathing.constants.*;
+
 import org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.Autonomous.AutonomousPLUS;
+import org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.Autonomous.AutonomousPlatinum;
 
 @Autonomous(group = "Basic", name = "Stupid Test")
 public class stupid_test extends AutonomousPLUS {
@@ -23,124 +28,156 @@ public class stupid_test extends AutonomousPLUS {
 
         super.runOpMode();
 
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
 
-        Follower follower;
-        Timer pathTimer;
-        Timer actionTimer;
-        Timer opmodeTimer;
+        Constants.setConstants(FConstants.class, LConstants.class);
+        follower = new Follower(hardwareMap);
+        follower.setStartingPose(startPose);
+        buildPaths();
 
-        /** This is the variable where we store the state of our auto.
-         * It is used by the pathUpdate method. */
-        int pathState;
+        waitForStart();
 
-        /* Create and Define Poses + Paths
-         * Poses are built with three constructors: x, y, and heading (in Radians).
-         * Pedro uses 0 - 144 for x and y, with 0, 0 being on the bottom left.
-         * (For Into the Deep, this would be Blue Observation Zone (0,0) to Red Observation Zone (144,144).)
-         * Even though Pedro uses a different coordinate system than RR, you can convert any roadrunner pose by adding +72 both the x and y.
-         * This visualizer is very easy to use to find and create paths/pathchains/poses: <https://pedro-path-generator.vercel.app/>
-         * Lets assume our robot is 18 by 18 inches
-         * Lets assume the Robot is facing the human player and we want to score in the bucket */
+        opmodeTimer.resetTimer();
+        setPathState(0);
 
-        /** Start Pose of our robot */
-        final Pose startPose = new Pose(2.75, 58, Math.toRadians(0)); // Basket parking is x = 2.75, y=109
+        //prepareAuto()
+        robot.encoderReset();
+        telemetry.addData(currentPosition,"Start position");
+        robot.liftyL.setPower(0);
+        robot.tempOutakePos("UP");
+        robot.intakePosition("UP");
+        speed = .3;
+        robot.outakeclawOpenClose("CLOSED");
 
-        /** Scoring Pose of our robot. It is facing the submersible at a -45 degree (315 degree) angle. */
-        final Pose scorePose = new Pose(14, 129, Math.toRadians(315));
+        prepareNextAction(4000);
 
-        /** Lowest (First) Sample from the Spike Mark */
-        final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0));
 
-        /** Middle (Second) Sample from the Spike Mark */
-        final Pose pickup2Pose = new Pose(43, 130, Math.toRadians(0));
+        do {
+            follower.followPath(scorePreload);
+        } while (follower.isBusy());
 
-        /** Highest (Third) Sample from the Spike Mark */
-        final Pose pickup3Pose = new Pose(49, 135, Math.toRadians(0));
+        moveArm(500,1,2000);
 
-        /** Park Pose for our robot, after we do all of the scoring. */
-        final Pose parkPose = new Pose(60, 98, Math.toRadians(90));
 
-        /** Park Control Pose for our robot, this is used to manipulate the bezier curve that we will create for the parking.
-         * The Robot will not go to this pose, it is used a control point for our bezier curve. */
-        final Pose parkControlPose = new Pose(60, 98, Math.toRadians(90));
 
-        /* These are our Paths and PathChains that we will define in buildPaths() */
-        Path scorePreload;
-        Path park;
-        PathChain grabPickup1, grabPickup2, grabPickup3, scorePickup1, scorePickup2, scorePickup3;
 
-        /** Build the paths for the auto (adds, for example, constant/linear headings while doing paths)
-         * It is necessary to do this so that all the paths are built before the auto starts. **/
-        public void buildPaths() {
 
-            /* There are two major types of paths components: BezierCurves and BezierLines.
-             *    * BezierCurves are curved, and require >= 3 points. There are the start and end points, and the control points.
-             *    - Control points manipulate the curve between the start and end points.
-             *    - A good visualizer for this is [this](https://pedro-path-generator.vercel.app/).
-             *    * BezierLines are straight, and require 2 points. There are the start and end points.
-             * Paths have can have heading interpolation: Constant, Linear, or Tangential
-             *    * Linear heading interpolation:
-             *    - Pedro will slowly change the heading of the robot from the startHeading to the endHeading over the course of the entire path.
-             *    * Constant Heading Interpolation:
-             *    - Pedro will maintain one heading throughout the entire path.
-             *    * Tangential Heading Interpolation:
-             *    - Pedro will follows the angle of the path such that the robot is always driving forward when it follows the path.
-             * PathChains hold Path(s) within it and are able to hold their end point, meaning that they will holdPoint until another path is followed.
-             * Here is a explanation of the difference between Paths and PathChains <https://pedropathing.com/commonissues/pathtopathchain.html> */
+    }
 
-            /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
-            scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePose)));
-            scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
+    /** Build the paths for the auto (adds, for example, constant/linear headings while doing paths)
+     * It is necessary to do this so that all the paths are built before the auto starts. **/
+    private Follower follower;
+    private Timer pathTimer, actionTimer, opmodeTimer;
+
+    /** This is the variable where we store the state of our auto.
+     * It is used by the pathUpdate method. */
+    private int pathState;
+
+    /* Create and Define Poses + Paths
+     * Poses are built with three constructors: x, y, and heading (in Radians).
+     * Pedro uses 0 - 144 for x and y, with 0, 0 being on the bottom left.
+     * (For Into the Deep, this would be Blue Observation Zone (0,0) to Red Observation Zone (144,144).)
+     * Even though Pedro uses a different coordinate system than RR, you can convert any roadrunner pose by adding +72 both the x and y.
+     * This visualizer is very easy to use to find and create paths/pathchains/poses: <https://pedro-path-generator.vercel.app/>
+     * Lets assume our robot is 18 by 18 inches
+     * Lets assume the Robot is facing the human player and we want to score in the bucket */
+
+    /** Start Pose of our robot */
+    private final Pose startPose = new Pose(9, 58, Math.toRadians(0)); // Basket parking is x = 9, y=109
+
+    /** Scoring Pose of our robot. It is facing the submersible at a -45 degree (315 degree) angle. */
+    private final Pose scorePose = new Pose(14, 129, Math.toRadians(315));
+
+    /** Lowest (First) Sample from the Spike Mark */
+    private final Pose pickup1Pose = new Pose(37, 121, Math.toRadians(0));
+
+    /** Middle (Second) Sample from the Spike Mark */
+    private final Pose pickup2Pose = new Pose(43, 130, Math.toRadians(0));
+
+    /** Highest (Third) Sample from the Spike Mark */
+    private final Pose pickup3Pose = new Pose(49, 135, Math.toRadians(0));
+
+    /** Park Pose for our robot, after we do all of the scoring. */
+    private final Pose parkPose = new Pose(60, 98, Math.toRadians(90));
+
+    /** Park Control Pose for our robot, this is used to manipulate the bezier curve that we will create for the parking.
+     * The Robot will not go to this pose, it is used a control point for our bezier curve. */
+    private final Pose parkControlPose = new Pose(60, 98, Math.toRadians(90));
+
+    /* These are our Paths and PathChains that we will define in buildPaths() */
+    private Path scorePreload, grabPickup1;
+    private Path park;
+    private PathChain grabPickup2, grabPickup3, scorePickup1, scorePickup2, scorePickup3;
+    private void buildPaths() {
+
+        /* There are two major types of paths components: BezierCurves and BezierLines.
+         *    * BezierCurves are curved, and require >= 3 points. There are the start and end points, and the control points.
+         *    - Control points manipulate the curve between the start and end points.
+         *    - A good visualizer for this is [this](https://pedro-path-generator.vercel.app/).
+         *    * BezierLines are straight, and require 2 points. There are the start and end points.
+         * Paths have can have heading interpolation: Constant, Linear, or Tangential
+         *    * Linear heading interpolation:
+         *    - Pedro will slowly change the heading of the robot from the startHeading to the endHeading over the course of the entire path.
+         *    * Constant Heading Interpolation:
+         *    - Pedro will maintain one heading throughout the entire path.
+         *    * Tangential Heading Interpolation:
+         *    - Pedro will follows the angle of the path such that the robot is always driving forward when it follows the path.
+         * PathChains hold Path(s) within it and are able to hold their end point, meaning that they will holdPoint until another path is followed.
+         * Here is a explanation of the difference between Paths and PathChains <https://pedropathing.com/commonissues/pathtopathchain.html> */
+
+        /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
+        scorePreload = new Path(new BezierLine(new Point(startPose), new Point(scorePose)));
+        scorePreload.setLinearHeadingInterpolation(startPose.getHeading(), scorePose.getHeading());
 
         /* Here is an example for Constant Interpolation
         scorePreload.setConstantInterpolation(startPose.getHeading()); */
 
-            /* This is our grabPickup1 PathChain. We are using a single path with a BezierLine, which is a straight line. */
-            grabPickup1 = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(scorePose), new Point(pickup1Pose)))
-                    .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
-                    .build();
+        /* This is our grabPickup1 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+        /*grabPickup1 = follower.pathBuilder()
+                .addPath(new BezierLine(new Point(scorePose), new Point(pickup1Pose)))
+                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading())
+                .build();*/
+        grabPickup1 = new Path(new BezierLine(new Point(scorePose), new Point(pickup1Pose)));
+        grabPickup1.setLinearHeadingInterpolation(scorePose.getHeading(), pickup1Pose.getHeading());
 
-            /* This is our scorePickup1 PathChain. We are using a single path with a BezierLine, which is a straight line. */
-            scorePickup1 = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(pickup1Pose), new Point(scorePose)))
-                    .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
-                    .build();
+        /* This is our scorePickup1 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+//        scorePickup1 = follower.pathBuilder()
+//                .addPath(new BezierLine(new Point(pickup1Pose), new Point(scorePose)))
+ //               .setLinearHeadingInterpolation(pickup1Pose.getHeading(), scorePose.getHeading())
+//                .build();
 
-            /* This is our grabPickup2 PathChain. We are using a single path with a BezierLine, which is a straight line. */
-            grabPickup2 = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(scorePose), new Point(pickup2Pose)))
-                    .setLinearHeadingInterpolation(scorePose.getHeading(), pickup2Pose.getHeading())
-                    .build();
+        /* This is our grabPickup2 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+//        grabPickup2 = follower.pathBuilder()
+//                .addPath(new BezierLine(new Point(scorePose), new Point(pickup2Pose)))
+//                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup2Pose.getHeading())
+//                .build();
 
-            /* This is our scorePickup2 PathChain. We are using a single path with a BezierLine, which is a straight line. */
-            scorePickup2 = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(pickup2Pose), new Point(scorePose)))
-                    .setLinearHeadingInterpolation(pickup2Pose.getHeading(), scorePose.getHeading())
-                    .build();
+        /* This is our scorePickup2 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+//        scorePickup2 = follower.pathBuilder()
+//                .addPath(new BezierLine(new Point(pickup2Pose), new Point(scorePose)))
+//                .setLinearHeadingInterpolation(pickup2Pose.getHeading(), scorePose.getHeading())
+//                .build();
 
-            /* This is our grabPickup3 PathChain. We are using a single path with a BezierLine, which is a straight line. */
-            grabPickup3 = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(scorePose), new Point(pickup3Pose)))
-                    .setLinearHeadingInterpolation(scorePose.getHeading(), pickup3Pose.getHeading())
-                    .build();
+        /* This is our grabPickup3 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+//        grabPickup3 = follower.pathBuilder()
+//                .addPath(new BezierLine(new Point(scorePose), new Point(pickup3Pose)))
+//                .setLinearHeadingInterpolation(scorePose.getHeading(), pickup3Pose.getHeading())
+//                .build();
 
-            /* This is our scorePickup3 PathChain. We are using a single path with a BezierLine, which is a straight line. */
-            scorePickup3 = follower.pathBuilder()
-                    .addPath(new BezierLine(new Point(pickup3Pose), new Point(scorePose)))
-                    .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose.getHeading())
-                    .build();
+        /* This is our scorePickup3 PathChain. We are using a single path with a BezierLine, which is a straight line. */
+//        scorePickup3 = follower.pathBuilder()
+//                .addPath(new BezierLine(new Point(pickup3Pose), new Point(scorePose)))
+//                .setLinearHeadingInterpolation(pickup3Pose.getHeading(), scorePose.getHeading())
+//                .build();
 
-            /* This is our park path. We are using a BezierCurve with 3 points, which is a curved line that is curved based off of the control point */
-            park = new Path(new BezierCurve(new Point(scorePose), /* Control Point */ new Point(parkControlPose), new Point(parkPose)));
-            park.setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading());
-        };
-
-        waitForStart();
-
-
-
-
-
+        /* This is our park path. We are using a BezierCurve with 3 points, which is a curved line that is curved based off of the control point */
+        park = new Path(new BezierCurve(new Point(scorePose), /* Control Point */ new Point(parkControlPose), new Point(parkPose)));
+        park.setLinearHeadingInterpolation(scorePose.getHeading(), parkPose.getHeading());
+    };
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
     }
 }
