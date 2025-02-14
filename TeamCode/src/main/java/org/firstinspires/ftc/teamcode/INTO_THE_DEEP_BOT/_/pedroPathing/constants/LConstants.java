bc672cdc9ee1e09005b8591e881675518adadeb6
ClaireToTheC0da <turtletastic998@gmail.com>
2025-02-13 20:29:58 -0600

    Updated linear scalar to more accurate value

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/pedroPathing/constants/LConstants.java
@@ -14,7 +14,7 @@ public class LConstants {
         OTOSConstants.linearUnit = DistanceUnit.INCH;
         OTOSConstants.angleUnit = AngleUnit.RADIANS;
         OTOSConstants.offset = new SparkFunOTOS.Pose2D(-5.23, -0.4866, 4.69769); // Heading isn't 0? y = -0.4866, x=-5.23
-        OTOSConstants.linearScalar = 1.199746552;
+        OTOSConstants.linearScalar = 1.074507936;
         OTOSConstants.angularScalar = 0.9953;
     }
 }
