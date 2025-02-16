cfcb4c98e578db971bed24a38c5cec8f65df7731
ClaireToTheC0da <turtletastic998@gmail.com>
2025-02-16 11:36:59 -0600

    Added descriptions for OTOS tuners

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/pedroPathing/tuners_tests/otos/OTOSPositionOffsetTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/_/pedroPathing/tuners_tests/otos/OTOSPositionOffsetTuner.java
@@ -9,6 +9,18 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.pedroPathing.constants.FConstants;
 import org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.pedroPathing.constants.LConstants;
 
+/**
+ * This is a pose offset tuner for the SparkFun OTOS, which is a method of localization supported by PedroPathing.
+ * It measures the change in the x and y values of the pose as the robot is rotated 180 degrees
+ * to return the true offset of the sensor from the dead center of the robot.
+ * <p></p>
+ * This is adapted from the SparkFun OTOS tuner written by Jay of 12087 Capital City Dynamics for RoadRunner.
+ *
+ * @author Emily Claire Lorenzen - 13206 Wave Droids, modified from Jay - 12087 Capital City Dynamics
+ * @version 1.0, 2/16/2025
+ */
+
+
 @TeleOp(name="OTOS Pose Offset Tuner", group="OTOS Tuning")
 public class OTOSPositionOffsetTuner extends LinearOpMode {
 
