0e3dff59e29b273dd5e51830d0d9a29d9b6c78e8
WSRWaveDroids <wavedroid13206@gmail.com>
2025-05-26 14:17:08 -0500

    Created files for middle school outreach tomorrow.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Outreach/middleSchool/group1.java
@@ -0,0 +1,31 @@
+package org.firstinspires.ftc.teamcode.Outreach.middleSchool;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import org.firstinspires.ftc.teamcode.INTO_THE_DEEP_BOT._.Autonomous.AutonomousPLUS;
+
+@Autonomous(group = "Basic", name = "Middle School Group 1")
+public class group1 extends AutonomousPLUS {
+
+    public String currentPosition;
+    public String target;
+
+    public void runOpMode() {
+
+        super.runOpMode();
+
+        waitForStart();
+        prepareAuto();
+        setMotorTolerance(10);
+
+        speed = .6;
+        sleep(200);
+
+        // Code goes here
+
+        robot.safeCollapse(); // Collapses the robot so the robot doesn't fall when auto ends
+        prepareNextAction(2000);
+
+
+    }
+}
