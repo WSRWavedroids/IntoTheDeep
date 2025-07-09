1e7b37b061067cd70864e2cd8382b5ef2ae16407
WSRWaveDroids <wavedroid13206@gmail.com>
2025-07-08 20:23:25 -0500

    Time to do some deleting!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Robot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/INTO_THE_DEEP_BOT/Robot.java
@@ -87,7 +87,7 @@ public class Robot {
         grabbyOutakeServoR = hardwareMap.get(Servo.class, "grabbyOutakeServoR");
         leftSlide = hardwareMap.get(Servo.class, "leftSlide");
         rightSlide = hardwareMap.get(Servo.class, "rightSlide");
-        //myOtos = hardwareMap.get(SparkFunOTOS.class, "sensor_otos"); //Uncomment for PAIN
+        //myOtos = hardwareMap.get(SparkFunOTOS.class, "sensor_otos");
 
 
         imuParameters = new IMU.Parameters(
